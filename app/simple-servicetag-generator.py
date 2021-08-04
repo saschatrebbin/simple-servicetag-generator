218652d267c9637d318bdbee25bb1a32630bcf2f
Sascha Trebbin <32403283+saschatrebbin@users.noreply.github.com>
2021-08-04 09:02:09 +0200

    added endpoint for short token generation and switched all endpoints to using the secrets module
    I need short tokens for combining entries in several online and offline tools and data storages and tie all those entries to a specific topic. Since the secrets module is perfectly suited for this task as well as the other random generations, I switched all endpoints from using random to using secrets.

--- app/simple-servicetag-generator.py
+++ app/simple-servicetag-generator.py
@@ -1,4 +1,6 @@
 import random
+import secrets
+
 from bottle import route, run
 
 secure_random = random.SystemRandom()
@@ -9,41 +11,46 @@ padding = 3
 
 @route('/v1/servicetag')
 @route('/v1/new')
-def hello():
+def generate():
 	basic_id_parts = []
-	basic_id_parts.append(secure_random.choice(prefixlist_international))
+	basic_id_parts.append(secrets.choice(prefixlist_international))
 	basic_id_parts.append("-")
-	basic_id_parts.append(str(secure_random.randint(-1,upper_limit)).zfill(3))
+	basic_id_parts.append(str(secrets.randbelow(1000)).zfill(3))
 	basic_id = "".join(basic_id_parts)
 	return "{0}".format(basic_id)
 
 @route('/v1/simplepassword')
-def hello():
+@route('/v1/simple-password')
+def generate():
 	basic_id_parts = []
-	basic_id_parts.append(secure_random.choice(prefixlist_international).capitalize())
+	basic_id_parts.append(secrets.choice(prefixlist_international).capitalize())
 	basic_id_parts.append("-")
-	basic_id_parts.append(secure_random.choice(prefixlist_international).capitalize())
+	basic_id_parts.append(secrets.choice(prefixlist_international).capitalize())
 	basic_id_parts.append("-")
-	basic_id_parts.append(str(secure_random.randint(-1,upper_limit)).zfill(3))
+	basic_id_parts.append(str(str(secrets.randbelow(1000)).zfill(3))
 	basic_id = "".join(basic_id_parts)
 	return "{0}".format(basic_id)
 
 @route('/v1/longpassword')
-def hello():
+@route('/v1/long-password')
+def generate():
 	basic_id_parts = []
-	basic_id_parts.append(secure_random.choice(prefixlist_international).capitalize())
+	basic_id_parts.append(secrets.choice(prefixlist_international).capitalize())
 	basic_id_parts.append("-")
-	basic_id_parts.append(secure_random.choice(prefixlist_international).capitalize())
+	basic_id_parts.append(secrets.choice(prefixlist_international).capitalize())
 	basic_id_parts.append("-")
-	basic_id_parts.append(secure_random.choice(prefixlist_international).capitalize())
+	basic_id_parts.append(secrets.choice(prefixlist_international).capitalize())
 	basic_id_parts.append("-")
-	basic_id_parts.append(secure_random.choice(prefixlist_international).capitalize())
+	basic_id_parts.append(secrets.choice(prefixlist_international).capitalize())
 	basic_id_parts.append("-")
-	basic_id_parts.append(secure_random.choice(prefixlist_international).capitalize())
+	basic_id_parts.append(secrets.choice(prefixlist_international).capitalize())
 	basic_id_parts.append("-")
-	basic_id_parts.append(str(secure_random.randint(-1,upper_limit)).zfill(3))
+	basic_id_parts.append(str(secrets.randbelow(1000)).zfill(3))
 	basic_id = "".join(basic_id_parts)
 	return "{0}".format(basic_id)
 
+@route('/v1/short-token')
+def generate():
+	return "{0}".format("#" + secrets.token_urlsafe(7))
 
 run(host='0.0.0.0', port=80, debug=True)
